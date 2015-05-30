f1081979b6d54df745b312fcc8edcc17b3b9880f
harabida <harabida@gmail.com>
2015-05-30 19:26:36 +0100

    Commit Mehdi : JsonUtila

---
+++ app/src/main/java/com/emergency/util/JsonUtils.java
@@ -0,0 +1,24 @@
+package com.emergency.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+
+/**
+ * Created by elmehdiharabida on 28/05/15.
+ */
+public class JsonUtils {
+
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    public static String toJson(Object objectIn) {
+
+        try {
+            return mapper.writeValueAsString(objectIn);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
